c6eff05b7bdeba1e9fed4118f10821e33e942428
jrob9308 <146874082+jrob9308@users.noreply.github.com>
2023-10-03 18:56:42 +0100

    Add files via upload

---
+++ gpu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Update package list and install necessary packages
+apt update
+apt -y install ocl-icd-opencl-dev
+apt -y install nano
+apt -y install htop
+apt -y install nvtop
+apt -y install cmake
+apt -y install python3-pip
+
+# Clone the repository and build the project
+git clone https://github.com/shanhaicoder/XENGPUMiner.git
+cd XENGPUMiner
+chmod +x build.sh
+./build.sh
+
+# Update the configuration file
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xe69554C8BE7148897ac924468D412E596a468E58/g' config.conf
+
+# Install Python requirements
+pip install -U -r requirements.txt
+nohup python miner.py --gpu=true > miner.log 2>&1 &
+nohup ./xengpuminer > xengpuminer.log 2>&1 &
---
+++ vast.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Update package list and install necessary packages
+sudo apt update
+sudo apt -y install ocl-icd-opencl-dev
+sudo apt -y install nano
+sudo apt -y install htop
+# sudo apt -y install nvtop
+sudo apt -y install cmake
+sudo apt -y install python3-pip
+
+# Clone the repository and build the project
+git clone https://github.com/shanhaicoder/XENGPUMiner.git
+cd XENGPUMiner
+chmod +x build.sh
+./build.sh
+
+# Update the configuration file
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+
+# Install Python requirements
+sudo pip install -U -r requirements.txt
+sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
+sudo nohup ./xengpuminer > xengpuminer.log 2>&1 &
